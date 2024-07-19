f44ff35e1b0d78997a7f2714bc4671ef25ebb2f6
Eduardik <badanov.edward@gmail.com>
2024-07-19 13:18:49 +0300

    create programm

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  let mainWindow = new BrowserWindow({
+    width: 800,
+    height: 500,
+    useContentSize: true,
+    resizable: false,
+    transparent: true,
+    frame: false,
+    focusable: true,
+    icon: __dirname + '/assets/favicon.png',
+    webPreferences: {
+      modeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
